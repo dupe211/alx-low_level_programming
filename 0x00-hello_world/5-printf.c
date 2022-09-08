37b7d15a44a8e5f2691c1238400cdd9b240c8340
Modupe Lawal <modupelawal2005@gmail.com>
2022-09-08 16:01:30 -0700

    Task 4-6 c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints the size of many var types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", size of(a));
+	printf("Size of an int: %d byte(s)\n", size of(b));
+	printf("Size of a long int: %d byte(s)\n", size of(c));
+	printf("Size of a long long int: %d byte(s)\n", size of(d));
+	printf("Size of a float: %d byte(s)\n", size of(e));
+	return (0);
+}
+
