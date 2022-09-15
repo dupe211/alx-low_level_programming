01bd661a30502fe5859dabecc2f43e4ebd586de1
Modupe Lawal <modupelawal2005@gmail.com>
2022-09-15 15:44:11 -0700

    main Task2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: 0;
+ * Return: 0
  */
 
 int main(void)
@@ -15,7 +15,7 @@ i = 0;
 /*
 *while (i < 10)
 *{
-*	putchar(i);
+*		putchar(i);
 *}
 */
 printf("Infinite loop avoided! \\o/n");
