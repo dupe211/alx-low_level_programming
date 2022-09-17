1ed0609e090bd29a653620902b9f26fecb06bb47
Modupe Lawal <modupelawal2005@gmail.com>
2022-09-16 19:48:46 -0700

    fivemore

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - a function that prints 10 times the numbers, from 0 to 14
  * _putchar only 3 times
- * Return: 0-14 x10 followed by new line
+ * Return: 0-14 x10 followed by new line.
  */
 void more_numbers(void)
 {
@@ -17,8 +17,8 @@ void more_numbers(void)
 			{
 				_putchar((i / 10) + '0');
 			}
-			_putchar((i % 10) + '0');
+			_putchar((i % 10) +'0');
 		}
 		_putchar('\n');
 	}
-}	
+}
